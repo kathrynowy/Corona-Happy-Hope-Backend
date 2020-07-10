fc3b79ae534fb8838483ee1ff4ba50fd47c27e9b
kathrynowy <weyulgex@gmail.com>
2020-07-10 23:26:22 +0300

    ready for heroku

--- index.ts
+++ index.ts
@@ -4,7 +4,7 @@ import app from './server';
 import mongoose from './src/context';
 
 const server = new http.Server(app);
-const port = process.env.PORT || 7000;
+const port = 7000;
 
 mongoose.connection.on('open', () => {
   server.listen(port, () => console.log(`Server is starting on port - ${port}`));
